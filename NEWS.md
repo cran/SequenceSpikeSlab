d6a341f86b8e6b1682553362db7b597ceeea3104
Tim van Erven <tim@timvanerven.nl>
2023-09-08 12:50:02 +0000

    version 1.0.1

---
+++ NEWS.md
@@ -0,0 +1,22 @@
+# SequenceSpikeSlab v1.0.1 (Release date: 2023-09-08)
+
+* Fix documentation package alias issue (see https://github.com/r-lib/roxygen2/issues/1491)
+* Switch from text NEWS to markdown NEWS.md to track changes to the package
+* Update CITATION to comply with new CRAN standards
+
+# SequenceSpikeSlab v1.0.0 (Release date: 2022-01-23)
+
+* Bump version number to signal stable release
+* Reference published version of Van Erven and Szabo paper in Bayesian
+  Analysis
+* Fix R CMD check warning about not importing namespace from RcppProgress
+* Switch maintainer
+
+# SequenceSpikeSlab v0.1.1 (Release date: 2019-12-17)
+
+* Fix C++ compilation warning on Solaris
+* Expand vignette
+
+# SequenceSpikeSlab v0.1 (Release date: 2019-12-04)
+
+* Initial release
--- R/SequenceSpikeSlab.R
+++ R/SequenceSpikeSlab.R
@@ -1,28 +1,3 @@
-# Make Roxygen add appropriate references for Rcpp when generating NAMESPACE
-#' @useDynLib SequenceSpikeSlab
-#' @importFrom Rcpp sourceCpp
-#' @import RcppProgress
-NULL
-
-# Add main documentation page for the whole package
-#' Fast Exact Bayesian Inference for the Sparse Normal Means Model
-#'
-#' The SequenceSpikeSlab package provides fast algorithms for exact Bayesian inference in the
-#' sparse normal sequence model. It implements the methods of Van Erven and Szabo, 2018.
-#' Special care has been taken to make the methods scale to large data sets, and to minimize
-#' numerical errors (which arise in all software because floating point numbers are
-#' represented with finite precision). 
-#' 
-#' There are two main functions:
-#' \code{\link{general_sequence_model}} and \code{\link{fast_spike_slab_beta}}.
-#' 
-#' For more details see the help vignette:
-#' \code{vignette("SequenceSpikeSlab-vignette", package="SequenceSpikeSlab")}
-#' 
-#' @docType package
-#' @name SequenceSpikeSlab
-NULL
-
 ################################################# MAIN WRAPPERS #####################################
 
 #' Compute marginal posterior estimates
